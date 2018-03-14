cf72a49b8cc802078267339350a9505388f02cac
unknown <jorovipe@gmail.com>
2018-03-14 18:30:37 -0500

    Transformation matrix training

---
+++ Assets/Matrix/Scripts/CameraTransformation.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraTransformation : Transformation {
+
+    public enum ProjectionType : byte
+    {
+        Orthographic,
+        Perspective
+    }
+
+    public ProjectionType projection = CameraTransformation.ProjectionType.Perspective;
+    
+    public override Matrix4x4 Matrix
+    {
+        get
+        {
+            Matrix4x4 matrix = new Matrix4x4();
+            matrix.SetRow(0, new Vector4(1f, 0f, 0f, 0f));
+            matrix.SetRow(1, new Vector4(0f, 1f, 0f, 0f));
+            matrix.SetRow(2, new Vector4(0f, 0f, 0f, 0f));
+            matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
+
+
+            if (projection == ProjectionType.Orthographic)
+            {
+                matrix.SetRow(3, new Vector4(0f, 0f, 0f, 1f));
+            }
+            else if (projection == ProjectionType.Perspective)
+            {
+                matrix.SetRow(3, new Vector4(0f, 0f, 1f, 0f));
+            }
+
+            return matrix;
+        }
+    }
+
+}
---
+++ Assets/Matrix/Scripts/TransformationGrid.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformationGrid : MonoBehaviour {
+
+    public Transform Prefab;
+    public int GridResolution;
+
+    private Transform[] grid;
+    private List<Transformation> transformations;
+    private Matrix4x4 transformation;
+
+	// Use this for initialization
+	void Awake () {
+        grid = new Transform[GridResolution * GridResolution * GridResolution];
+        for (int i = 0, z = 0; z < GridResolution; z++)
+        {
+            for (int y = 0; y < GridResolution; y++)
+            {
+                for (int x = 0; x < GridResolution; x++, i++)
+                {
+                    grid[i] = CreateGridPoint(x, y, z);
+                }
+            }
+        }
+
+        transformations = new List<Transformation>();
+    }   
+
+    // Update is called once per frame
+    void Update () {
+        UpdateTransformation();
+        for (int i = 0, z = 0; z < GridResolution; z++)
+        {
+            for (int y = 0; y < GridResolution; y++)
+            {
+                for (int x = 0; x < GridResolution; x++, i++)
+                {
+                    grid[i].localPosition = TransformPoint(x, y, z);
+                }
+            }
+        }
+    }
+
+    void UpdateTransformation()
+    {
+        GetComponents<Transformation>(transformations);
+        if (transformations.Count > 0)
+        {
+            transformation = transformations[0].Matrix;
+            for (int i = 1; i < transformations.Count; i++)
+            {
+                transformation = transformations[i].Matrix * transformation;
+            }
+        }
+    }
+
+    private Transform CreateGridPoint(int x, int y, int z)
+    {
+        Transform point = Instantiate<Transform>(Prefab);
+        point.localPosition = GetCoordinates(x, y, z);
+        point.GetComponent<MeshRenderer>().material.color = new Color(
+            (float)x / GridResolution,
+            (float)y / GridResolution,
+            (float)z / GridResolution
+        );
+        return point;
+    }
+
+    private Vector3 GetCoordinates(int x, int y, int z)
+    {
+        return new Vector3(
+            x - (GridResolution - 1) * 0.5f,
+            y - (GridResolution - 1) * 0.5f,
+            z - (GridResolution - 1) * 0.5f
+        );
+    }
+
+    private Vector3 TransformPoint(int x, int y, int z)
+    {
+        Vector3 coordinates = GetCoordinates(x, y, z);
+        return transformation.MultiplyPoint(coordinates); ;
+    }
+}
